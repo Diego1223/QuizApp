c452be1b23d50c984fae5acecc25bdc0ac90f269
Diego1223 <diegonn2555@gmail.com>
2021-09-05 11:35:30 -0500

    1.1

---
+++ QuizApp/Juego.h
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string.h>
+
+struct Logica
+{
+	std::string preguntas[50] = {
+		"Que es un compilador?"
+		"Lenguaje que consiste de ceros y unos",
+		"Lenguaje de programacion creado en 1979 por Bjarne Stroustrup",
+		"Lenguaje de programacion creado en 1996 por la empresa Sun Microsystems"
+	};
+
+	std::string respuestas[50] = {
+		"Software que traduce un programa de un lenguaje de alto nivel a un lenguaje maquina",
+		"Lenguaje binario",
+		"Lenguaje C++",
+		"Lenguaje java"
+	};
+}pregunta, respuesta;
